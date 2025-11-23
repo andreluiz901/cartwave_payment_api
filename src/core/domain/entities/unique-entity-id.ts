97333c18903f2a95f28c0d53f591e1fe2d3df464
André <silva.ala82@gmail.com>
2025-11-23 15:27:27 -0300

    feat: implement domain unique entity id

---
+++ src/core/domain/entities/unique-entity-id.ts
@@ -0,0 +1,23 @@
+import { InvalidUuidError } from '@/core/errors/invalid-uuid.error';
+import { v4 as uuidV4, validate as uuidValidate } from 'uuid';
+
+export class UniqueEntityId {
+  private readonly value: string;
+
+  constructor(id?: string) {
+    this.value = id || uuidV4();
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.value);
+
+    if (!isValid) {
+      throw new InvalidUuidError(this.value);
+    }
+  }
+
+  toString() {
+    return this.value;
+  }
+}
---
+++ src/core/errors/invalid-uuid.error.ts
@@ -0,0 +1,6 @@
+export class InvalidUuidError extends Error {
+  constructor(id?: string) {
+    super(`Invalid UUID: ${id}`);
+    this.name = 'InvalidUuidError';
+  }
+}
